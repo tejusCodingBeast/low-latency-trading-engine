b6263eae4830204db3b0cefebd22dee1fc01c1fc
Nigel Saurino <nigelsaurino@Nigels-MacBook-Air.local>
2024-01-11 19:55:07 -0600

    Need to update CMake for Order classes and am starting the order class implementations

---
+++ IOrderCore.h
@@ -0,0 +1,26 @@
+#ifndef IORDERCORE_H
+#define IORDERCORE_H
+#include <string>
+
+class IOrderCore{
+    private:
+    int order_id_;
+    std::string username_;
+    int security_id_;
+
+    public:
+    // Constructors
+    IOrderCore(const int order_id, const std::string username, const int security_id){
+        order_id_ = order_id;
+        username_ = username;
+        security_id_ = security_id;
+    }
+    
+    // Pure virtual member functions
+    virtual int GetOrderID() const = 0;
+    virtual std::string GetUsername() const = 0;
+    virtual int GetSecurityID() const = 0;
+    
+};
+
+#endif
---
+++ OrderCore.h
@@ -0,0 +1,20 @@
+#ifndef ORDERCORE_H
+#define ORDERCORE_H
+#include "IOrderCore.h"
+
+class OrderCore: public IOrderCore{
+    private:
+
+
+    public:
+    // Constructors
+    OrderCore(const int order_id, const std::string username, const int security_id);
+
+    // Member Functions
+    int GetOrderID() const;
+    std::string GetUsername();
+    int GetSecurityID();
+
+};
+
+#endif
